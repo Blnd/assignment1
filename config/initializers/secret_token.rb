c8b878e134be29a0a7d12e83446760ef8147e531
blnd <blnd.najmadin@auis.edu.iq>
2013-03-25 08:51:22 -0700

    db change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assignment1::Application.config.secret_token = '3863c48f8a1acb1c057da0fa4f39e9dba9e824e9324e58b7fd97ff1616f493e73f6dc418a184fbb5b216fdf91252bf3a7d92324958debbd4d3c14a07430b6fa2'
